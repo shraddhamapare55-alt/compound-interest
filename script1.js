40ede270d9579c42bcf1a692722d35db2c254fd2
shraddhamapare55-alt <shraddhamapare55@gmail.com>
2026-05-29 12:15:51 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,6 +1,6 @@
-const P = 100000;      
-const r = 0.05;        
-const n = 1;           
-const t = 3;           
-const A = P * Math.pow((1 + (r / n)), (n * t));
-console.log("The compound interest after " + t + " years is: " + Math.round(A-P));
+let P = 40000;
+let r = 0.08; // 8%
+let n = 1;
+let t = 3;
+let A = P * (1 + r / n) ** (n * t);
+console.log("The total amount after " + t + " years is: " + A.toFixed(2));
